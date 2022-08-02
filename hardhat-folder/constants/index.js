1417eec59f6f67d927e0f4114516d19962e555a3
Roysignaler <roysignaler@gmail.com>
2022-08-02 05:48:34 +0200

    initial commit

---
+++ hardhat-folder/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x30132add9e379E3A01092f0Cf8f9F11eF7b54044";
+// URL to extract Metadata for a ITU Dev NFT
+const METADATA_URL = "https://itu-tests-nft-collection.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
